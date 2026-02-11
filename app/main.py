2747059cb96c1788126829fabc823573ddd9c64b
Tee Kai Yang <tkaiyang2005@gmail.com>
2026-02-12 00:28:00 +0800

    Uncomment relevant code

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.include_router(chat.router)
 app.include_router(convo.router)
-# app.include_router(summary.router)
+app.include_router(summary.router)
 
 app.add_middleware(
     CORSMiddleware,
